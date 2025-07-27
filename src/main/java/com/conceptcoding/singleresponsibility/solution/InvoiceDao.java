4ad510e35016aa1d087be45588298581cbcc08c1
AASHRITA BILIANGADI <aashrita.biliangadi@gmail.com>
2025-07-28 00:47:02 +0530

    SRP - initial commit

---
+++ src/main/java/com/conceptcoding/singleresponsibility/solution/InvoiceDao.java
@@ -0,0 +1,16 @@
+package com.conceptcoding.singleresponsibility.solution;
+
+// Responsibility 2: Managing Database Operations only
+public class InvoiceDao {
+
+    Invoice invoice;
+
+    public InvoiceDao(Invoice invoice) {
+        this.invoice = invoice;
+    }
+
+    public void saveToDB() {
+        // Save into the DB the invoice
+        System.out.println("Saving to DB...");
+    }
+}
