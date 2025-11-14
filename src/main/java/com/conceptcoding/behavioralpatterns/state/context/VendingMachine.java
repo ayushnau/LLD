8a818e06da4f98ea1ba1928b833bacebf1f1285d
Shrayansh Jain <shrayanshjain@Shrayanshs-MacBook-Pro.local>
2025-11-14 17:21:04 +0530

    minor change

--- src/main/java/com/conceptcoding/behavioralpatterns/state/context/VendingMachine.java
+++ src/main/java/com/conceptcoding/behavioralpatterns/state/context/VendingMachine.java
@@ -7,41 +7,41 @@ import com.conceptcoding.behavioralpatterns.state.vendingmachinestates.impl.Idle
 import java.util.ArrayList;
 import java.util.List;
 
-public class VendingMachine {
+    public class VendingMachine {
 
-    private State vendingMachineState;
-    private Inventory inventory;
-    private List<Coin> coinList;
+        private State vendingMachineState;
+        private Inventory inventory;
+        private List<Coin> coinList;
 
-    public VendingMachine(){
-        vendingMachineState = new IdleState();
-        inventory = new Inventory(10);
-        coinList = new ArrayList<>();
+        public VendingMachine(){
+            vendingMachineState = new IdleState();
+            inventory = new Inventory(10);
+            coinList = new ArrayList<>();
+        }
+
+        public State getVendingMachineState() {
+            return vendingMachineState;
+        }
+
+        public void setVendingMachineState(State vendingMachineState) {
+            this.vendingMachineState = vendingMachineState;
+        }
+
+        public Inventory getInventory() {
+            return inventory;
+        }
+
+        public void setInventory(Inventory inventory) {
+            this.inventory = inventory;
+        }
+
+        public List<Coin> getCoinList() {
+            return coinList;
+        }
+
+        public void setCoinList(List<Coin> coinList) {
+            this.coinList = coinList;
+        }
     }
-
-    public State getVendingMachineState() {
-        return vendingMachineState;
-    }
-
-    public void setVendingMachineState(State vendingMachineState) {
-        this.vendingMachineState = vendingMachineState;
-    }
-
-    public Inventory getInventory() {
-        return inventory;
-    }
-
-    public void setInventory(Inventory inventory) {
-        this.inventory = inventory;
-    }
-
-    public List<Coin> getCoinList() {
-        return coinList;
-    }
-
-    public void setCoinList(List<Coin> coinList) {
-        this.coinList = coinList;
-    }
-}
 
 
